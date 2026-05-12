f0f32f36272197c0328b5dfbb628962a3c7bc12a
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-12 10:56:56 -0700

    first commit

--- APIAutomation/Demo1GetTest.cs
+++ APIAutomation/Demo1GetTest.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using KPMG.APIAutomation.Support;
+using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -24,6 +25,7 @@ namespace KPMG.APIAutomation
 
             //convert fron stream json to object
             dynamic petResponse = JsonConvert.DeserializeObject(response.Content);
+            Console.WriteLine(petResponse);
             Console.WriteLine(petResponse.id);
             Console.WriteLine(petResponse.category.id);
 
@@ -53,6 +55,54 @@ namespace KPMG.APIAutomation
             foreach(var item in petArrayResponse)
             {
                 Assert.That(Convert.ToString(item.status), Is.EqualTo(status));
+            }
+        }
+
+        /// <summary>
+        /// Deserialize to Pet Object 
+        /// </summary>
+        [Test]
+        public void FindValidPetById2Test()
+        {
+            var client = new RestClient(baseUrl: "https://petstore.swagger.io/v2");
+            var request = new RestRequest("pet/{petId}", Method.Get);
+
+            request.AddUrlSegment("petId", 105);
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content);
+
+            var petResponse = JsonConvert.DeserializeObject<Pet>(response.Content);
+            Console.WriteLine(petResponse);
+
+            Console.WriteLine(petResponse.Id);
+            Console.WriteLine(petResponse.Category.Id);
+
+        }
+        /// <summary>
+        /// Deserialize to List<Pet> Object 
+        /// </summary>
+        [TestCase("available")]
+        public void FindPetByStatus2Test(string status)
+        {
+            var client = new RestClient(baseUrl: "https://petstore.swagger.io/v2");
+            var request = new RestRequest("pet/findByStatus", Method.Get);
+            request.AddQueryParameter("status", status);
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content);
+
+            var petArrayResponse = JsonConvert.DeserializeObject<List<Pet>>(response.Content);
+            Console.WriteLine(petArrayResponse[0].Status);
+
+            //use foreach and assert each status to be sold
+            foreach (var item in petArrayResponse)
+            {
+                Assert.That(Convert.ToString(item.Status), Is.EqualTo(status));
             }
         }
     }
---
+++ APIAutomation/Support/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KPMG.APIAutomation.Support
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Tag
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Pet
+    {
+        public long Id { get; set; }
+        public Category Category { get; set; }
+        public string Name { get; set; }
+        public List<string> PhotoUrls { get; set; }
+        public List<Tag> Tags {  get; set; }
+        public string Status { get; set; }
+
+    }
+}
