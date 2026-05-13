397e8b34c58d47d713d3074d3d6925b20f6904fa
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-13 10:27:56 -0700

    added schema validation using NJsonSchema

---
+++ APIAutomation/Demo4SchemaValidation.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using NJsonSchema;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KPMG.APIAutomation
+{
+    public class Demo4SchemaValidation
+    {
+        [Test]
+        public async Task GetPetSchemaValidationTest()
+        {
+            var client = new RestClient(baseUrl: "https://petstore.swagger.io/v2");
+            var request = new RestRequest("pet/{petId}", Method.Get);
+            request.AddUrlSegment("petId", 705);
+
+            var response = client.Execute(request);
+
+            //parse the schema and validate response against the schema
+            var schema= await JsonSchema.FromJsonAsync(File.ReadAllText(@"testdata\petschema.json"));
+            var errors = schema.Validate(JToken.Parse(response.Content));
+
+            Console.WriteLine(errors);
+
+            if(errors.Any())
+            {
+                foreach(var error in errors)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("valid!");
+            }
+        }
+    }
+}
